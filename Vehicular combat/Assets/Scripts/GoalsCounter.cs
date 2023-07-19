05dbc724d1dd8f5d3a5bd40c64a501db4506afe3
Juan Ignacio Almada <almada.ignacio2002@gmail.com>
2023-07-19 17:21:10 -0300

    UpdateGoalsCounter

--- Vehicular combat/Assets/Scripts/GoalsCounter.cs
+++ Vehicular combat/Assets/Scripts/GoalsCounter.cs
@@ -26,8 +26,16 @@ public class GoalsCounter : MonoBehaviour
     {
         if( gameManager.ScoreGoal == 1)
         {
-            Time.timeScale = 0;
             Cursor.lockState = CursorLockMode.None;
+
+            GameObject.Find("|||||PLAYER||||||").GetComponent<SimpleController>().enabled = enabled;
+            GameObject.Find("|||||PLAYER||||||").GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX;
+            GameObject.Find("Car").GetComponent<Opponent>().enabled = false;
+            GameObject.Find("TMP counter").GetComponent<Timer>().enabled = false;
+            GameObject.Find("reloj").GetComponent<Animator>().enabled = false;
+            GameObject.Find("vacio aguja").GetComponent<VisualClockl>().enabled = false;
+            GameObject.Find("Image izq").GetComponent<Animator>().enabled = false;
+            GameObject.Find("Image der").GetComponent<Animator>().enabled = false;
 
             panel.SetActive(true);
         }
@@ -42,7 +50,14 @@ public class GoalsCounter : MonoBehaviour
 
         if (other.gameObject.CompareTag("Car1"))
         {
-            Time.timeScale = 0;
+            GameObject.Find("|||||PLAYER||||||").GetComponent<SimpleController>().enabled = enabled;
+            GameObject.Find("|||||PLAYER||||||").GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX;
+            GameObject.Find("Car").GetComponent<Opponent>().enabled = false;
+            GameObject.Find("TMP counter").GetComponent<Timer>().enabled = false;
+            GameObject.Find("reloj").GetComponent<Animator>().enabled = false;
+            GameObject.Find("vacio aguja").GetComponent<VisualClockl>().enabled = false;
+            GameObject.Find("Image izq").GetComponent<Animator>().enabled = false;
+            GameObject.Find("Image der").GetComponent<Animator>().enabled = false;
             Cursor.lockState = CursorLockMode.None;
             panel2.SetActive(true);
         }
